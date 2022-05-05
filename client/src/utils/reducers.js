e33c01f8b8d406bb6370f6e42aca87ef4b87c6a9
Devon Faria <devonfaria@gmail.com>
2022-05-05 12:18:47 -0400

    added Details.js to handle actions and reducers

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,23 +1,23 @@
 import { useReducer } from "react";
 import {
-  UPDATE_PRODUCTS,
+  // UPDATE_PRODUCTS,
   ADD_TO_CART,
-  UPDATE_CART_QUANTITY,
+  // UPDATE_CART_QUANTITY,
   REMOVE_FROM_CART,
-  ADD_MULTIPLE_TO_CART,
-  UPDATE_CATEGORIES,
-  UPDATE_CURRENT_CATEGORY,
+  // ADD_MULTIPLE_TO_CART,
+  // UPDATE_CATEGORIES,
+  // UPDATE_CURRENT_CATEGORY,
   CLEAR_CART,
   TOGGLE_CART
 } from "./actions";
 
 export const reducer = (state, action) => {
   switch (action.type) {
-    case UPDATE_PRODUCTS:
-      return {
-        ...state,
-        products: [...action.products],
-      };
+    // case UPDATE_PRODUCTS:
+    //   return {
+    //     ...state,
+    //     products: [...action.products],
+    //   };
 
     case ADD_TO_CART:
       return {
@@ -26,23 +26,23 @@ export const reducer = (state, action) => {
         cart: [...state.cart, action.product],
       };
 
-    case ADD_MULTIPLE_TO_CART:
-      return {
-        ...state,
-        cart: [...state.cart, ...action.products],
-      };
+    // case ADD_MULTIPLE_TO_CART:
+    //   return {
+    //     ...state,
+    //     cart: [...state.cart, ...action.products],
+    //   };
 
-    case UPDATE_CART_QUANTITY:
-      return {
-        ...state,
-        cartOpen: true,
-        cart: state.cart.map(product => {
-          if (action._id === product._id) {
-            product.purchaseQuantity = action.purchaseQuantity
-          }
-          return product
-        })
-      };
+    // case UPDATE_CART_QUANTITY:
+    //   return {
+    //     ...state,
+    //     cartOpen: true,
+    //     cart: state.cart.map(product => {
+    //       if (action._id === product._id) {
+    //         product.purchaseQuantity = action.purchaseQuantity
+    //       }
+    //       return product
+    //     })
+    //   };
 
     case REMOVE_FROM_CART:
       let newState = state.cart.filter(product => {
@@ -68,17 +68,17 @@ export const reducer = (state, action) => {
         cartOpen: !state.cartOpen
       };
 
-    case UPDATE_CATEGORIES:
-      return {
-        ...state,
-        categories: [...action.categories],
-      };
+    // case UPDATE_CATEGORIES:
+    //   return {
+    //     ...state,
+    //     categories: [...action.categories],
+    //   };
 
-    case UPDATE_CURRENT_CATEGORY:
-      return {
-        ...state,
-        currentCategory: action.currentCategory
-      }
+    // case UPDATE_CURRENT_CATEGORY:
+    //   return {
+    //     ...state,
+    //     currentCategory: action.currentCategory
+    //   }
 
     default:
       return state;
