7bce426295930e97ba754a5d82c76270192375f1
Guru Darji <gurudarji@ymail.com>
2022-05-06 13:58:58 -0400

    editted productPage

--- client/src/pages/ProductPage.js
+++ client/src/pages/ProductPage.js
@@ -1,24 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_BOX } from '../utils/queries';
 import { useStoreContext } from "../utils/GlobalState";
-import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../utils/actions";
+import { ADD_TO_CART, UPDATE_CART_QUANTITY, UPDATE_PRODUCTS } from "../utils/actions";
 import { idbPromise } from "../utils/helpers";
 import Cart from "../components/Cart";
+import { QUERY_BOXES } from '../utils/queries';
 
 
 const ProductPage = () => {
   const [state, dispatch] = useStoreContext();
-  const { cart } = state;
+  const { id } = useParams();
+  const { cart, products } = state;
+  
+  const [currentProduct, setCurrentProduct] = useState({});
 
+  const { loading, data } = useQuery(QUERY_BOXES);
+
+  useEffect(() => {
+    // already in global store
+    if (products.length) {
+      setCurrentProduct(products.find((product) => product._id === id));
+    }
+    // retrieved from server
+    else if (data) {
+      dispatch({
+        type: UPDATE_PRODUCTS,
+        products: data.products,
+      });
+
+      data.boxes.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
+    }
+    // get cache from idb
+    else if (!loading) {
+      idbPromise('products', 'get').then((indexedProducts) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: indexedProducts,
+        });
+      });
+    }
+  }, [products, data, loading, dispatch, id]);
+
+  const { boxId } = useParams();
+  const { loading: loadingBox, data: dataBox } = useQuery(QUERY_BOX, {
+    variables: { boxId: boxId },
+  });
 
   const addToCart = () => {
-    const itemInCart = cart.find((cartItem) => cartItem._id === box.id)
+    const itemInCart = cart.find((cartItem) => cartItem._id === boxId)
     if (itemInCart) {
       dispatch({
         type: UPDATE_CART_QUANTITY,
-        _id: box.id,
+        _id: boxId,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
       });
       idbPromise('cart', 'put', {
@@ -28,22 +65,18 @@ const ProductPage = () => {
     } else {
       dispatch({
         type: ADD_TO_CART,
-        product: { ...box, purchaseQuantity: 1 }
+        product: { ...currentProduct, purchaseQuantity: 1 }
       });
-      idbPromise('cart', 'put', { ...box, purchaseQuantity: 1 });
+      idbPromise('cart', 'put', { ...currentProduct, purchaseQuantity: 1 });
     }
   }
 
-  const { boxId } = useParams();
-  const { loading, data } = useQuery(QUERY_BOX, {
-    variables: { boxId: boxId },
-  });
 
-  const box = data?.box;
+  const box = dataBox?.box;
 
   const breakline = '\u000A';
 
-  if (loading) {
+  if (loadingBox) {
     return <span>Loading</span>;
   }
   return (
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -6,7 +6,7 @@ import { ADD_USER } from '../utils/mutations';
 
 import Auth from '../utils/auth';
 
-const Signup = () => {
+const Signup = (props) => {
   const [formState, setFormState] = useState({
     username: '',
     email: '',
