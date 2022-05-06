d0a33465a83262fc54e6622b3637c303b3c0fe81
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2022-05-06 15:20:08 -0400

    deleted extra pages that were duplicates

--- client/src/App.js
+++ client/src/App.js
@@ -13,10 +13,8 @@ import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import Profile from './pages/Profile';
-import MyAccount from './pages/MyAccount';
 import Checkout from './pages/Checkout';
 import AboutUs from './pages/AboutUs';
-import Box from './pages/Box';
 import Categories from './pages/Categories';
 import OrderConfirmation from './pages/OrderConfirmation';
 import ProductPage from './pages/ProductPage';
@@ -66,9 +64,7 @@ function App() {
                 <Route path="/aboutus" element={<AboutUs />} />
                 <Route path="/checkout" element={<Checkout />} />
                 <Route path="/orderconfirmation" element={<OrderConfirmation />} />
-                <Route path="/myaccount" element={<MyAccount />} />
                 <Route path="/categories" element={<Categories />} />
-                <Route path="/box" element={<Box />} />
               </Routes>
             </StoreProvider>
           </div>
