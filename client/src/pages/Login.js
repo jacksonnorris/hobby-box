0d5776e8bdf86a9f0e38bfade1f4c483915dd95b
Devon Faria <devonfaria@gmail.com>
2022-05-08 19:25:24 -0400

    corrected form required functionality and rendering, and added extra margin above form inputs

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -6,12 +6,27 @@ import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const Login = () => {
-  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [formState, setFormState] = useState({
+    email: '',
+    password: ''
+  });
   const [login, { error, data }] = useMutation(LOGIN_USER);
 
   // update state based on form input changes
   const handleChange = (event) => {
     const { name, value } = event.target;
+    const { target } = event;
+
+    if (value.length === 0) {
+      target.classList.add('missing');
+      const required = target.nextElementSibling
+      required.removeAttribute('hidden');
+    } else {
+      target.classList.remove('missing');
+      const removed = target.nextElementSibling
+      console.log(removed);
+      removed.setAttribute('hidden', 'hidden');
+    }
 
     setFormState({
       ...formState,
@@ -49,10 +64,10 @@ const Login = () => {
       )
     }
     return (
-      <div className='my-5
+      <div className='my-12
       form-control
       block
-      w-6/12
+      w-4/12
       px-3
       py-3
       text-base
@@ -66,8 +81,8 @@ const Login = () => {
       m-0
       focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
     "'>
-      <h2 className='loginTitle'>Login</h2>
-        <form onSubmit={handleFormSubmit}>
+        <h2 className='loginTitle'>Login</h2>
+        <form class='form-login' onSubmit={handleFormSubmit}>
           <div className='loginFormInputs'>
             <input
               placeholder="Email"
@@ -76,6 +91,9 @@ const Login = () => {
               value={formState.email}
               onChange={handleChange}
             />
+            <div className="form-warning" hidden='hidden'>
+              <p className='warning mt-2' hidden>Required</p>
+            </div>
           </div>
           <div className='loginFormInputs'>
             <input
@@ -85,6 +103,9 @@ const Login = () => {
               value={formState.password}
               onChange={handleChange}
             />
+            <div className="form-warning" hidden='hidden'>
+              <p className='warning mt-2' hidden>Required</p>
+            </div>
           </div>
           <div className='loginSubmitButton'>
             <button type="submit" class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400   to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
@@ -100,7 +121,7 @@ const Login = () => {
 
   return (
     <main className='SandLBackground'>
-      <div className='container mx-auto px-4 my-8 marginSetter'>
+      <div className='container mx-auto px-4'>
         <div className='loginForm'>
           {renderForm()}
         </div>
