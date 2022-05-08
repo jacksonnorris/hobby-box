347750a7d15eac88ac251b02dfcc9d56f945133e
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2022-05-08 11:42:42 -0400

    changed background colors on nav links and background color on shopping cart icon

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -66,7 +66,7 @@ const Cart = () => {
 
   if (!state.cartOpen) {
     return (
-      <div className="cart-closed text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" onClick={toggleCart}>
+      <div className="cart-closed text-white bg-gradient-to-r from-teal-500 via-teal-200 to-teal-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" onClick={toggleCart}>
         <span role="img" aria-label="trash">
           🛒
         </span>
